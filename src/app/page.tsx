9bc83a0c3b5560d908a7dae2e48d9507abc1c651
dev_weslei <wesleyhprozao@gmail.com>
2026-01-21 00:29:18 -0400

    chore: add shadCn on it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="p-5 ">
+      <Button>Bootcamp Clinic</Button>
     </div>
   );
 }
